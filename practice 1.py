ff8f2b2a2107ad565a4c00b0af88887de178be96
Hoorain630 <hoorainsaud19@gmail.com>
2024-12-09 21:59:29 +0500

    Add files via upload

---
+++ practice 1.py
@@ -0,0 +1,7 @@
+
+print("1. mix 500g of flour and 10 ml of water in a bowl")
+print("2.knead the dough for 10 min")
+print("3.add 3 grams of salt")
+print("4.leave it to rise for 2 hours")
+print("5.bake at 200 degree celcius for 30 min")
+print("she said: \"hello\" and then left")
