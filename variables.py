80d59cdd27f8df010666d1096c8930984cdb6e41
Hoorain630 <hoorainsaud19@gmail.com>
2024-12-12 21:37:36 +0500

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,10 @@
+a="hello"
+b="world"
+print("a=",a)
+print("b=",b)
+
+print("welcome to the band name generator")
+city=input("enter the city you live in = ")
+pet_name=input("enter you pet name = ")
+band_name=city+pet_name
+print(band_name)
