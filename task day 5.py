f74249399c2bded0027ef1a0a5172df3cc2ce660
Hoorain630 <hoorainsaud19@gmail.com>
2024-12-16 22:10:32 +0500

    Add files via upload

---
+++ task day 5.py
@@ -0,0 +1,23 @@
+
+#task 1
+age=int(input("enter your age = "))
+years=int(input("enter the years u think u will live  = "))
+difference= age - years
+months= difference*12
+weeks=months*4
+print(f"you have (weeks) left in ur life")
+
+#tip calculator 
+
+print("welcome to the tip calculator")
+total_bill=float(input("what was the total bill = "))
+people=int(input("how many people would like to split the bill = "))
+tip=("how much tip would u like to give?= ")
+percentage=tip/100
+new_amount=total_bill + percentage
+bill_division=new_amount/people
+print("each person should pay :")
+
+
+
+
