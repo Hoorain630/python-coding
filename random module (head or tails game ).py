e20b6cb91b52c31e9f81139f57b85cf9932bc54f
Hoorain630 <hoorainsaud19@gmail.com>
2024-12-20 21:02:59 +0500

    Add files via upload

---
+++ random module (head or tails game ).py
@@ -0,0 +1,11 @@
+
+
+import random
+
+Heads=1
+Tails=0
+game = random.randint(0,1)
+if game == 0:
+    print("Tails")
+else:
+    print("Heads")
---
+++ random module (random pick from list to pay the bill).py
@@ -0,0 +1,9 @@
+
+
+
+import random
+
+list=["hoorain","manahil","zarmeen","fatima","khadija","aleeha"]
+random_pick = random.choice(list)
+print(f"{random_pick} is going to pay the bill for todays lunch")
+
